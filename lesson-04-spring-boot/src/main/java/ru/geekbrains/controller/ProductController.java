6ac44dbf558ffae74b5c292e26edc98d4ddb1fb8
Yuriy <yurgennik@gmail.com>
2022-04-06 17:25:38 +0500

    JavaEE-lesson4-spring boot

---
+++ lesson-04-spring-boot/src/main/java/ru/geekbrains/controller/ProductController.java
@@ -0,0 +1,54 @@
+package ru.geekbrains.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import ru.geekbrains.persist.Product;
+import ru.geekbrains.persist.ProductRepository;
+
+@RequestMapping("/product")
+@Controller
+public class ProductController {
+
+    private final ProductRepository productRepository;
+
+    @Autowired
+    public ProductController(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    @GetMapping
+    public String listPage(Model model) {
+        model.addAttribute("products", productRepository.findAll());
+        return "product";
+    }
+
+    @GetMapping("/{id}")
+    public String form(@PathVariable("id") long id, Model model) {
+        model.addAttribute("product", productRepository.findById(id));
+        return "product_form";
+    }
+
+    @GetMapping("/new")
+    public String newProduct(Model model) {
+        // TODO
+        model.addAttribute("product",new Product(""));
+//        productRepository.save(product);
+        return "product_form";
+    }
+
+    @PostMapping
+    public String save(Product product) {
+        productRepository.save(product);
+        return "redirect:/product";
+    }
+    @GetMapping("/del/{id}")
+    public String delete(@PathVariable("id") long id){
+       productRepository.delete(id);
+       return "redirect:/product";
+    }
+}
---
+++ lesson-04-spring-boot/src/main/java/ru/geekbrains/persist/Product.java
@@ -0,0 +1,48 @@
+package ru.geekbrains.persist;
+
+public class Product {
+
+    private Long id;
+
+    private String title;
+
+    private Long coast;
+
+    private String description;
+
+    public Product(String title) {
+        this.title = title;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Long getCoast() {
+        return coast;
+    }
+
+    public void setCoast(Long coast) {
+        this.coast = coast;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
