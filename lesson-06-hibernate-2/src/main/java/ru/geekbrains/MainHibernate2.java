381de60edbe1620b98db2e928c75b9f4c606e40f
Yuriy <yurgennik@gmail.com>
2022-04-17 19:21:34 +0500

    JavaEE-lesson6-hibernate2 home work

--- lesson-06-hibernate-2/src/main/java/ru/geekbrains/MainHibernate2.java
+++ lesson-06-hibernate-2/src/main/java/ru/geekbrains/MainHibernate2.java
@@ -1,29 +1,42 @@
 package ru.geekbrains;
 
+import org.hibernate.Session;
+import ru.geekbrains.DAO.UserDAO;
+import ru.geekbrains.DAO.UserDAOImpl;
 import ru.geekbrains.model.Product;
 import ru.geekbrains.DAO.ProductDAO;
 import ru.geekbrains.DAO.ProductDAOImpl;
+import ru.geekbrains.model.User;
 import ru.geekbrains.utils.SessionFactoryUtils;
 
 import java.util.List;
 
-public class Main {
+public class MainHibernate2 {
 
     public static void main(String[] args) {
 
         SessionFactoryUtils sessionFactoryUtils = new SessionFactoryUtils();
         sessionFactoryUtils.init();
-
+        Session session = null;
         try {
 
            ProductDAO productDAO = new ProductDAOImpl(sessionFactoryUtils);
+            UserDAO userDAO = new UserDAOImpl(sessionFactoryUtils);
+
+
+//           User user = userDAO.findById(1l);
+//            System.out.println(user);
+//           List<Product> p = user.getProducts();
+//            p.forEach(System.out::println);
+
+            Product product = productDAO.findById(1l);
+            System.out.println(product);
+            List<User> u = product.getUsers();
+            u.forEach(System.out::println);
 
 //            Product product = productDAO.findById(2l);
 //            product.print();
 
-//            productDAO.deleteById(2l);
-           List<Product> product = productDAO.findAll();
-            product.forEach(System.out::println);
 
 //            Product product = productDAO.saveOrUpdate(new Product("Product2","solid",200l));
 //            product.print();
